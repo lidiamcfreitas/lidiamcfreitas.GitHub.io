3c8398e4854f033e719f00b27bf324858afdc711
lidiamcfreitas <lidiamcfreitas@gmail.com>
2015-05-14 14:27:39 +0100

    update

--- js/dragdealer.js
+++ js/dragdealer.js
@@ -615,14 +615,13 @@ Dragdealer.prototype = {
         Cursor.y - this.offset.wrapper[1] - this.offset.mouse[1]
 
       ];
-      if(navigator.userAgent.indexOf("Firefox") != -1 ) 
-    {
+      if(navigator.userAgent.indexOf("Firefox") != -1 ) {
          console.log('Firefox');
          offset[0] *=2;
          offset[1] *=2;
-    } else {
+       } else {
             console.log("other browser");   
-      }
+       }
 
       this.setTargetValueByOffset(offset, this.options.loose);
 
